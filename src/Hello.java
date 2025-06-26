c29958e940ccb9a3f4bdcd4973f7bbc3d1202e02
dotmons <dotmons@gmail.com>
2025-06-26 15:14:38 -0400

    Create a new bind

--- src/Hello.java
+++ src/Hello.java
@@ -3,7 +3,8 @@ package example;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 
-@WebService
+@WebService(name = "hello", 
+            targetNamespace = "http://example.com/hello")
 public class Hello {
     @WebMethod
     public String sayHello(String name) {
